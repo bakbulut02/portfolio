715d08c9007b6b34a051a98705acc10514304429
Beliz Akbulut <bakbulut@ucsd.edu>
2026-04-15 18:18:29 -0700

    checking

--- global.js
+++ global.js
@@ -17,7 +17,7 @@ document.body.prepend(nav);
 
 const BASE_PATH = (location.hostname === "localhost") || location.hostname === "127.0.0.1"
     ? "/"
-    : "/portfolio/";
+    : "/bakbulut02.github.io/portfolio/";
 
 for (let p of pages) {
     let url=p.url;
