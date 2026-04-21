82684adb3f5703f24824fa924a91009e92db4e69
Beliz Akbulut <bakbulut@ucsd.edu>
2026-04-21 15:02:05 -0700

    working on lab4

--- global.js
+++ global.js
@@ -96,3 +96,36 @@ form?.addEventListener('submit', function(event){
     location.href = url;
 
 });
+
+
+export async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data', error);
+    }
+}
+
+
+export function renderProjects(projects, containerElement) {
+    containerElement.innerHTML = '';
+    for (const proj of projects) {
+        const article = document.createElement('article');
+        
+        article.innerHTML = `
+            <h3>${proj.title}</h3>
+            <img src="${proj.image}" alt="${proj.title}">
+            <p>${proj.description}</p>
+        `;
+        containerElement.appendChild(article);
+    }
+}
+
+// export function renderProjects(project, containerElement, headingLevle = 'h2') {
+//     //erm
+// }
